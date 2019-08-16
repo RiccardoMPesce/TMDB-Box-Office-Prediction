2ffebfcab71c97a3df9763aa838a71d405867538
RiccardoMPesce <riccardompesce@gmail.com>
2019-08-16 21:52:25 +0200

    Adds encoding

--- tmdb.py
+++ tmdb.py
@@ -56,11 +56,77 @@ for col in train_df.columns:
 
 
 #%%
+pred_id = 3001
 cols_to_drop = ["id", "title", "homepage", "imdb_id", "original_title", "overview", "poster_path", "tagline"]
 
 train_df = train_df.drop(cols_to_drop, axis=1)
 test_df = test_df.drop(cols_to_drop, axis=1)
 
 # Displaying converted data
+with pd.option_context("display.max_columns", None):
+    display(train_df.head())
+
+#%% [markdown]
+# #### Transforming columns whose elements are dict
+#
+# Some columns have dicts as values. Such dicts contains different informations, but we want to just retain the identifying one (in most, this is the *name*). For this reason, we are going to map these column values to the extracted name from the dicts.
+#
+# For such objective, we will use `pandas`'s method `map()`. We map to the *name* value of the dicts of the column *genres*. For *spoken_languages*, we will map to the keys *iso_639_1*'s values.
+#
+# To avoid potential malfunctionings, we fill NA values of non numerical features with the class "None", and later, during a further step, we will appropiately choose the right class for such missing features.
+
+#%%
+import ast
+import math
+
+cat_cols = train_df.select_dtypes(exclude="number").columns
+
+train_df[cat_cols] = train_df[cat_cols].fillna("None")
+test_df[cat_cols] = test_df[cat_cols].fillna("None")
+
+train_df["genres"] = train_df["genres"].apply(lambda x: ast.literal_eval(x)[0]["name"] if x != "None" else "None")
+test_df["genres"] = test_df["genres"].apply(lambda x: ast.literal_eval(x)[0]["name"] if x != "None" else "None")
+
+train_df["spoken_languages"] = train_df["spoken_languages"].apply(lambda x: ast.literal_eval(x)[0]["iso_639_1"] if x != "None" else "None")
+test_df["spoken_languages"] = test_df["spoken_languages"].apply(lambda x: ast.literal_eval(x)[0]["iso_639_1"] if x != "None" else "None")
+
+# Displaying converted data
+with pd.option_context("display.max_columns", None):
+    display(train_df.head())
+
+#%% [markdown]
+# #### What should we do about features that could potentially span into lots of indicator variables?
+# Initially, we can think that the feature *Keywords* is an important one, since it refers to the feature of a film itself, and so should be very descriptive about it. However, these are just used by imdb so they won't confer so much accuracy to our predictions. Therefore, we are going to drop them, also because mapping every possible keyword to an indicator variable could increase the complexity of our model, and therefore it could overfit the data.
+#
+# We are also dropping, for the same reason, the features *belongs_to_collection*, *original_language*, *production_companies*, *production_countries*, *cast* and *crew*. These variables, unlike the previous one, could tell us potentially useful informations for inferential analyses. However, they will span out into lot of classes, and during test, there will be some classes, spanned into indicators, that our model hasn't learned, and so, it will be a problem.
+
+#%%
+train_df = train_df.drop(["belongs_to_collection", "original_language", "production_companies", "production_countries", "spoken_languages", "Keywords", "cast", "crew"], axis=1)
+test_df = test_df.drop(["belongs_to_collection", "original_language", "production_companies", "production_countries", "spoken_languages", "Keywords", "cast", "crew"], axis=1)
+
+# Displaying pruned data
+with pd.option_context("display.max_columns", None):
+    display(train_df.head())
+
+
+#%% [markdown]
+# #### Encoding
+# Now we have to encode our categorical variables. The variable *status* can be encoded with 1/0, while *release_date* can be simply transformed to POSIX timestamp for easy conversion.
+
+#%%
+from sklearn.preprocessing import OneHotEncoder
+enc = OneHotEncoder(handle_unknown="ignore")
+
+train_df["status"] = train_df["status"].apply(lambda x: 1 if x == "Released" else 0)
+test_df["status"] = test_df["status"].apply(lambda x: 1 if x == "Released" else 0)
+
+train_df["release_date"] = train_df["release_date"].apply(lambda x: x.timestamp())
+test_df["release_date"] = test_df["release_date"].apply(lambda x: x.timestamp())
+
+enc.fit(train_df)
+
+enc.transform(train_df)
+enc.transform(test_df)
+
 with pd.option_context("display.max_columns", None):
     display(train_df.head())

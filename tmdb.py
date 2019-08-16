133cc2a6f2f209fa981708c67db57437f63c8667
RiccardoMPesce <riccardompesce@gmail.com>
2019-08-16 11:41:54 +0200

    Adds data folder

---
+++ tmdb.py
@@ -0,0 +1,48 @@
+#%% [markdown]
+# TMDB: Box Office Prediction
+# **[Link to the challenge](https://www.kaggle.com/c/tmdb-box-office-prediction)**
+#
+# ## Description
+# We're going to make you an offer you can't refuse: a Kaggle competition!
+#
+# In a world… where movies made an estimated $41.7 billion in 2018, the film industry is more popular than ever. But what movies make the most money at the box office? How much does a director matter? Or the budget? For some movies, it's "You had me at 'Hello.'" For others, the trailer falls short of expectations and you think "What we have here is a failure to communicate."
+#
+# In this competition, you're presented with metadata on over 7,000 past films from The Movie Database to try and predict their overall worldwide box office revenue. Data points provided include cast, crew, plot keywords, budget, posters, release dates, languages, production companies, and countries. You can collect other publicly available data to use in your model predictions, but in the spirit of this competition, use only data that would have been available before a movie's release.
+#
+# Join in, "make our day", and then "you've got to ask yourself one question: 'Do I feel lucky?'"
+
+#%% [markdown]
+# ## Exploratory Data Analysis
+#
+# Without further ado, let's first view the dataset, print out the predictors columns and try to get the first insight of the data.
+
+#%%
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+# Using seaborn to plot
+import seaborn as sns
+# Using os.path.join() to create system-independent paths
+import os
+
+# Defining where our data is located (for more than one folders, using a list)
+DATA_FOLDER = ["..", "input"]
+TRAIN_FILE = "train.csv"
+TEST_FILE = "test.csv"
+
+# Creating file paths
+train_path = os.path.join(*DATA_FOLDER, TRAIN_FILE)
+test_path = os.path.join(*DATA_FOLDER, TEST_FILE)
+
+# Loading data with pandas (parsing date columns appropriately)
+train_df = pd.read_csv(train_path, parse_dates=["release_date"])
+test_df = pd.read_csv(test_path, parse_dates=["release_date"])
+
+with pd.option_context("display.max_columns", None):
+    display(train_df.head())
+
+# Printing shape and columns of the data
+print(train_df.shape)
+print(train_df.columns)
+
+#%%
